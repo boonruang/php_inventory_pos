db439a259044067e3890c4a518b80fc3fb01ada1
boonruang <scubatoy@gmail.com>
2022-09-17 16:29:41 +0700

    46. Login code part-01

--- connectdb.php
+++ connectdb.php
@@ -1,5 +1,12 @@
 <?php
 
-$pdo = new PDO('mysql:host=localhost;dbname=pos_db','root','');
-echo 'Connection Successfull'
+try{
+ $pdo = new PDO('mysql:host=localhost;dbname=pos_db','root','');
+// echo 'Connection Successfull';   
+     
+} catch(PDOException $f){
+    echo $f->getmessage();
+}
+
+
 ?>
