c75d22403e4e487fcfbba7ee5f0f952c229a6544
boonruang <scubatoy@gmail.com>
2022-09-18 13:39:57 +0700

    60. Registration form part-03-formname-attr

--- registration.php
+++ registration.php
@@ -36,36 +36,37 @@ if ($_SESSION['role'] == 'Admin') {
         | Your Page Content Here |
         -------------------------->
         
-          <div class="box box-primary">
+          <div class="box box-info">
             <div class="box-header with-border">
               <h3 class="box-title">Registration Form</h3>
             </div>
             <!-- /.box-header -->
             <!-- form start -->
-            <form role="form">
+            <form role="form" action="" method="post">
               <div class="box-body">
                
             <div class="col-md-4">
                
                 <div class="form-group">
-                  <label for="exampleInputEmail1">Name</label>
-                  <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter Name">
+                  <label >Name</label>
+                  <input type="text" class="form-control" name="txtname" placeholder="Enter Name" name>
                 </div>
                                
                 <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                  <label >Email address</label>
+                  <input type="email" class="form-control" name="txtemail" placeholder="Enter email">
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputPassword1">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  <label >Password</label>
+                  <input type="password" class="form-control" name="txtpassword" placeholder="Password">
                 </div>
                 
                 <div class="form-group">
                   <label>Role</label>
-                  <select class="form-control">
-                    <option>Admin</option>
+                  <select class="form-control" name="txtselect_option">
+                    <option value="" disabled selected>Select role</option>
                     <option>User</option>
+                    <option>Admin</option>
 
                   </select>
                 </div>                
@@ -81,29 +82,31 @@ if ($_SESSION['role'] == 'Admin') {
                         <th>EMAIL</th>
                         <th>PASSWORD</th>
                         <th>ROLE</th>
+                        <th>DELETE</th>
                     </tr>
                 </thead>
                 <tbody>
 
-<?php
-$select = $pdo->prepare("select * from tbl_user order by userid desc");
-$select->execute();
+            <?php
+            $select = $pdo->prepare("select * from tbl_user order by userid desc");
+            $select->execute();
 
-                    
 
-while($row = $select->fetch(PDO::FETCH_OBJ)) {
-   echo '
-    <tr>
-        <td>'.$row->userid.'</td>
-        <td>'.$row->username.'</td>
-        <td>'.$row->useremail.'</td>
-        <td>'.$row->password.'</td>
-        <td>'.$row->role.'</td>                        
-    </tr>     
-    '; 
-}
 
-?>                    
+            while($row = $select->fetch(PDO::FETCH_OBJ)) {
+               echo '
+                <tr>
+                    <td>'.$row->userid.'</td>
+                    <td>'.$row->username.'</td>
+                    <td>'.$row->useremail.'</td>
+                    <td>'.$row->password.'</td>
+                    <td>'.$row->role.'</td>  
+                    <td></td>
+                </tr>     
+                '; 
+            }
+
+            ?>                    
                                                               
           
                 </tbody>
@@ -117,7 +120,7 @@ while($row = $select->fetch(PDO::FETCH_OBJ)) {
               <!-- /.box-body -->
 
               <div class="box-footer">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-info" name="btnregister">Save</button>
               </div>
             </form>
           </div>    
